48508ae4af6804816d4b2ca575911ce2733b880a
Leo <leo07carrasco@gmail.com>
2023-03-09 16:14:00 -0300

    i added lodash unescape for the question

--- src/component/Question.jsx
+++ src/component/Question.jsx
@@ -1,11 +1,15 @@
+import unescape from "lodash.unescape"
 
 const Question = ({question}) => {
 
   // console.log(question.replace(/&quot;/g, '"'))
+    const a = question.replace(/&#039;/g,"'" );
+    const strUnescape = unescape(a);
+
     return (
       <div className='text-4xl text-center mb-10 mt-5 relative px-10'>
         <h1 className="leading-normal">
-          <span className="text-7xl text-green-500 font-bold  absolute left-0 animate-bounce -top-2.5">¿</span>{question}<span className="text-7xl text-green-500 font-bold animate-bounce absolute right-0">?</span>
+          <span className="text-7xl text-green-500 font-bold  absolute left-0 animate-bounce -top-2.5">¿</span>{strUnescape}<span className="text-7xl text-green-500 font-bold animate-bounce absolute right-0">?</span>
         </h1>
       </div>
     )
