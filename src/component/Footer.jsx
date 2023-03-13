56b21884b6cd46b7f13031d7740c99f3f5505d39
Leo <leo07carrasco@gmail.com>
2023-03-13 15:12:38 -0300

    links

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,7 @@ import { FaGithub } from "react-icons/fa";
 const Footer = () => {
     return (
         <footer className="py-2 text-center">
-            <a href="" className="flex justify-center items-center gap-2 hover:text-gray-400 ease-in-out text-base">Made by Leonardo A.Carrasco <FaGithub /></a>
+            <a href="https://github.com/LeonardoCarrasco" className="flex justify-center items-center gap-2 hover:text-gray-400 ease-in-out text-base">Made by Leonardo A.Carrasco <FaGithub /></a>
         </footer>
     )
 }
--- src/component/Socials.jsx
+++ src/component/Socials.jsx
@@ -3,9 +3,9 @@ import { FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 const Socials = () => {
     return (
         <div className="flex justify-center items-center gap-3 text-2xl">
-            <a href="" className="hover:scale-110 ease-in-out"><FaTwitter /></a>
-            <a href="" className="hover:scale-110 ease-in-out"><FaInstagram /></a>
-            <a href="" className="hover:scale-110 ease-in-out"><FaLinkedin /></a>
+            <a href="https://twitter.com/iLeoCarrasco" className="hover:scale-110 ease-in-out"><FaTwitter /></a>
+            <a href="https://www.instagram.com/ileocarrasco/" className="hover:scale-110 ease-in-out"><FaInstagram /></a>
+            <a href="https://www.linkedin.com/in/leonardo-carrasco-5026b8246/" className="hover:scale-110 ease-in-out"><FaLinkedin /></a>
         </div>
     )
 }
